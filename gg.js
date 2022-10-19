7df29da011a2767be2095534c90e54cafd63fbde
hfharsono <114998121+hfharsono@users.noreply.github.com>
2022-10-19 18:06:51 +0700

    Add files via upload

---
+++ gg.js
@@ -0,0 +1,9 @@
+
+let checkGanjilDanGenap = (angka) => {
+    if (angka % 2 == 0){
+        console.log("bilangan " + angka + " adalah genap")
+    } else {
+        console.log("Bilangan " + angka + " adalah ganjil")
+    }
+}
+checkGanjilDanGenap(91)
